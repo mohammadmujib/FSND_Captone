0ddc8f1f75d09d2a4f30894e20c960e4e36a4f96
Mohammadmujib <moahammadmujib09@gmail.com>
2020-04-23 12:40:12 +0530

    Update Setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 export AUTH0_DOMAIN='capstone-casting.auth0.com'
 export AUTH0_CLIENTID='1WOTxcL9BI2MY7bF3poP8bfTWh6o4ZnN'
 export API_AUDIENCE='casting'
-export DATABASE_URL='postgres://bxkqhxbcaxeops:9ad284d1da2f0102316269dd24a2ebd6686bf854520a4602e55936e79ff47751@ec2-18-233-137-77.compute-1.amazonaws.com:5432/d4j1v6ibcqurrc'
+export DATABASE_URL='postgres://vlaiicyuvfyxsu:0c8d2719a0bfb2a622a78302d13cadf31c365bfe35b609b25bd7fa2e09b1e1a5@ec2-23-20-129-146.compute-1.amazonaws.com:5432/dcet34fm3mi6cq'
 export FLASK_APP=app.py
 export FLASK_DEBUG=true
 flask run --reload
